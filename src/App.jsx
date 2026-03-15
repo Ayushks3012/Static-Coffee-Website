ecb4996cad9b2a0e3e90f3ccbdf542fc41983ce1
Ayushks3012 <ayushks628@gmail.com>
2026-03-16 00:54:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Discover from './components/Discover'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <Discover />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Discover.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Discover = () => {
+  return (
+    <section className='relative bg-gradient-to-r from-[#f1e0c5] to-[#4b3838]min-h-[50vh] text-[#4e342e] font-serif py-12 px-4 flex items-center overflow-hidden'> 
+      <div className='container max-w-7xl mx-auto w-full flex flex-col md:flex-row gap-8'>
+        {/* Left side */}
+        <div className=''>
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Discover
